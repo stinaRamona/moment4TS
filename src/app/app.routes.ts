d322f22214c36db08a0181bc0b075f87cfc61f73
Stina Persson <stpe1901@student.miun.se>
2024-04-25 16:39:33 +0200

    Gjort component för startsida och routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MycoursesComponent } from './mycourses/mycourses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "mycourses", component: MycoursesComponent },
+    { path: " ", redirectTo: "mycourses", pathMatch: 'full'}
+];
---
+++ src/app/mycourses/mycourses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycourses',
+  standalone: true,
+  imports: [],
+  templateUrl: './mycourses.component.html',
+  styleUrl: './mycourses.component.css'
+})
+export class MycoursesComponent {
+
+}
