99988e304b8f90c8cc21348853e758b267d436d6
Stina Persson <stpe1901@student.miun.se>
2024-04-26 14:51:35 +0200

    Man kan filtrera kurser, både kurskod och kursnamn

--- src/app/mycourses/mycourses.component.ts
+++ src/app/mycourses/mycourses.component.ts
@@ -2,24 +2,37 @@ import { Component } from '@angular/core';
 import { Courseinfo } from '../models/courseinfo';
 import { CoursesService } from '../services/courses.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mycourses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './mycourses.component.html',
   styleUrl: './mycourses.component.css'
 })
 export class MycoursesComponent {
 
   //kurser sparas och initieras till tom array 
-  courses: Courseinfo[] = [];  
+  courses: Courseinfo[] = [];
+  //efter filtrering sparas de också i denna  
+  filteredCourses: Courseinfo[] = []; 
+  //för det som skrivs in i inputfältet 
+  filtervalue: string = "" 
 
   constructor(private coursesService: CoursesService) { }
 
   ngOnInit(){
     this.coursesService.getCourses().subscribe((courses) => {
       this.courses = courses
+      this.filteredCourses = courses
     })
+  }
+
+  applyFilter(): void {
+    //filtrerar kurskod och kursnamn
+    this.filteredCourses = this.courses.filter((course)=>
+      course.code && course.coursename.toLowerCase().includes(this.filtervalue.toLowerCase())   
+    )
   }
 }
